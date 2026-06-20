880b395b6abab8d0f47ad22b0e660e81a9a4f199
ArthurHebertt <you@example.com>
2026-06-19 22:31:29 -0300

    first commit

---
+++ src/main/java/dev/ProjetoTeste/Sistema/de/Estoque/de/Produtos/SistemaDeEstoqueDeProdutosApplication.java
@@ -0,0 +1,13 @@
+package dev.ProjetoTeste.Sistema.de.Estoque.de.Produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeEstoqueDeProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeEstoqueDeProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ProjetoTeste/Sistema/de/Estoque/de/Produtos/SistemaDeEstoqueDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ProjetoTeste.Sistema.de.Estoque.de.Produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeEstoqueDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
