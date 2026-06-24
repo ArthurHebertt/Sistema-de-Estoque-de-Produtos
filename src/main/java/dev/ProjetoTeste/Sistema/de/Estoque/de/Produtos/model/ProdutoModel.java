7f3b1fcde310b265e878f86cc8db9a33f5256a7f
ArthurHebertt <you@example.com>
2026-06-23 23:18:11 -0300

    feat: cria a Model/Entity de produtos da API

--- src/main/java/dev/ProjetoTeste/Sistema/de/Estoque/de/Produtos/model/ProdutoModel.java
+++ src/main/java/dev/ProjetoTeste/Sistema/de/Estoque/de/Produtos/model/ProdutoModel.java
@@ -1,4 +1,68 @@
 package dev.ProjetoTeste.Sistema.de.Estoque.de.Produtos.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_estoque")
 public class ProdutoModel {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String descricao;
+
+    private Integer preco;
+
+    private int quantidade;
+
+    public ProdutoModel(Long id, String nome, String descricao, Integer preco, int quantidade) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Integer preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 }
