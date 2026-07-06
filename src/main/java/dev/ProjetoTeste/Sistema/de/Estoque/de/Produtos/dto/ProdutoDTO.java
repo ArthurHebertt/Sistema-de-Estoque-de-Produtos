37c363df51dff9089b850843984281218d5fe337
ArthurHebertt <you@example.com>
2026-07-05 22:33:04 -0300

    feat: cria o mapper e o DTO para entidade Produtos

---
+++ src/main/java/dev/ProjetoTeste/Sistema/de/Estoque/de/Produtos/dto/ProdutoDTO.java
@@ -0,0 +1,4 @@
+package dev.ProjetoTeste.Sistema.de.Estoque.de.Produtos.dto;
+
+public class ProdutoDTO {
+}
---
+++ src/main/java/dev/ProjetoTeste/Sistema/de/Estoque/de/Produtos/mapper/ProdutoMapper.java
@@ -0,0 +1,4 @@
+package dev.ProjetoTeste.Sistema.de.Estoque.de.Produtos.mapper;
+
+public class ProdutoMapper {
+}
