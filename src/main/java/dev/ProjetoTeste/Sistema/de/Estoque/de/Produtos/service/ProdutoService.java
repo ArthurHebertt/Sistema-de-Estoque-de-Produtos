ef87b30afc5dce26d9c106570bfc5bdc21c9db18
ArthurHebertt <you@example.com>
2026-07-08 23:22:16 -0300

    fix: inclui DTO como abstração da entidade na camada de Service e Controller

--- src/main/java/dev/ProjetoTeste/Sistema/de/Estoque/de/Produtos/service/ProdutoService.java
+++ src/main/java/dev/ProjetoTeste/Sistema/de/Estoque/de/Produtos/service/ProdutoService.java
@@ -1,35 +1,46 @@
 package dev.ProjetoTeste.Sistema.de.Estoque.de.Produtos.service;
 
+import dev.ProjetoTeste.Sistema.de.Estoque.de.Produtos.dto.ProdutoDTO;
+import dev.ProjetoTeste.Sistema.de.Estoque.de.Produtos.mapper.ProdutoMapper;
 import dev.ProjetoTeste.Sistema.de.Estoque.de.Produtos.model.ProdutoModel;
 import dev.ProjetoTeste.Sistema.de.Estoque.de.Produtos.repository.ProdutoRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @Service
 public class ProdutoService {
 
     public ProdutoRepository produtoRepository;
+    private ProdutoMapper produtoMapper;
 
-    public ProdutoService(ProdutoRepository produtoRepository) {
+    public ProdutoService(ProdutoRepository produtoRepository, ProdutoMapper produtoMapper) {
         this.produtoRepository = produtoRepository;
+        this.produtoMapper = produtoMapper;
     }
 
     // Adicionar produto
-    public ProdutoModel adicionarProduto (ProdutoModel produtoModel) {
-        return produtoRepository.save(produtoModel);
+    public ProdutoDTO adicionarProduto (ProdutoDTO produtoDTO) {
+        ProdutoModel produto = produtoMapper.map(produtoDTO);
+       produto = produtoRepository.save(produto);
+       return produtoMapper.map(produto);
     }
 
     // Listar produtos
-    public List<ProdutoModel> listaDeProdutos() {
-        return produtoRepository.findAll();
+    public List<ProdutoDTO> listaDeProdutos() {
+        List<ProdutoModel> produtos = produtoRepository.findAll();
+        return produtos.stream()
+                .map(produtoMapper::map)
+                .collect(Collectors.toList());
     }
 
     // Listar produtos por ID
-    public ProdutoModel listarPorId(Long id) {
-         return produtoRepository.findById(id)
-               .orElseThrow(() -> new RuntimeException("Produto não encontraado"));
+    public ProdutoDTO listarPorId(Long id) {
+        Optional <ProdutoModel> produto = produtoRepository.findById(id);
+         return produto.map(produtoMapper::map).orElse(null);
    }
 //  outra altertativa para procurar por ID
 //    public ProdutoModel listarPorId(Long id) {
@@ -46,11 +57,16 @@ public class ProdutoService {
 
     // Editar produto
 
-    public ProdutoModel editarProduto(Long id, ProdutoModel produto) {
-        produtoRepository.findById(id).orElseThrow(()-> new RuntimeException("Produto não encontrado"));
-        produto.setId(id);
-        return produtoRepository.save(produto);
+    public ProdutoDTO editarProduto (Long id, ProdutoDTO produtoDTO) {
+        Optional<ProdutoModel> produtoExistente = produtoRepository.findById(id);
+        if (produtoExistente.isPresent()) {
+            ProdutoModel produtoEditado = produtoMapper.map(produtoDTO);
+            produtoEditado.setId(id);
+            ProdutoModel produtoSalvo = produtoRepository.save(produtoEditado);
+            return produtoMapper.map(produtoSalvo);
+        } return null;
     }
+
 
     // Deletar produto
     public void deletarProduto(Long id) {
